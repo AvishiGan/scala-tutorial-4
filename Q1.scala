959d765b949fd03c5210ced528c6949f6b1738db
Avishi Gan <avishiganepola@gmail.com>
2022-08-02 08:49:30 +0530

    Changes made Q1

--- Q1.scala
+++ Q1.scala
@@ -5,8 +5,8 @@ object Q1 extends App{
         case x if x<0 => 0
         case x if x<20000 => x*0.02
         case x if x<200000 => x*0.04
-        case x if x<2000000 => x*0.35
-        case x if x>=200000 => x*0.65
+        case x if x<2000000 => x*0.035
+        case x if x>=200000 => x*0.065
     }
     println(interest(100))
 }
